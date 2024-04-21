32a73718f9202cb5620bb30b59f49c97c6d28dc9
spawar955 <spawar869089@gmail.com>
2024-04-22 02:33:49 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import AllRoutes from "./Routes/AllRoutes";
 import { ToastContainer } from "react-toastify";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://deploy-mern-api.vercel.app/register', {name, email, password})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
   return (
     <div className="App">
       <AllRoutes />
